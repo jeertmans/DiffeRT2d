898950aadb8ec64a630a07d993ecc71d621089ba
Jérome Eertmans <jeertmans@icloud.com>
2023-06-14 19:16:29 +0200

    chore(docs): chaning theme to awesomesphinx
    Because why not

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,11 @@
 import os
 import sys
 
+from dataclasses import asdict
+
+from sphinxawesome_theme import ThemeOptions
+
+
 project = "DiffeRT2d"
 copyright = "2023, Jérome Eertmans"
 author = "Jérome Eertmans"
@@ -30,6 +35,11 @@ extensions = [
     "sphinx_copybutton",
     "plot_directive",
 ]
+
+html_context = {
+    "mode": "production",
+    "feedback_url": "https://github.com/kai687/sphinxawesome-theme/issues/new?title=Feedback",
+}
 
 rst_epilog = """
 .. role:: python(code)
@@ -67,28 +77,45 @@ plot_rst_directory = "source/apidocs/differt2d/"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+html_theme = "sphinxawesome_theme"
 # html_static_path = ["_static"]
 
-html_theme_options = {
-    "light_logo": "logo_light_transparent.png",
-    "dark_logo": "logo_dark_transparent.png",
-    "footer_icons": [
-        {
-            "name": "GitHub",
-            "url": "https://github.com/jeertmans/DiffeRT2d",
-            "html": """
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-                </svg>
-            """,
-            "class": "",
+theme_options = ThemeOptions(
+    show_prev_next=True,
+    awesome_external_links=True,
+    main_nav_links={"Docs": "/index", "About": "/about", "Changelog": "/changelog"},
+    extra_header_link_icons={
+        "repository on GitHub": {
+            "link": "https://github.com/jeertmans/DiffeRT2d",
+            "icon": (
+                '<svg height="26px" style="margin-top:-2px;display:inline" '
+                'viewBox="0 0 45 44" '
+                'fill="currentColor" xmlns="http://www.w3.org/2000/svg">'
+                '<path fill-rule="evenodd" clip-rule="evenodd" '
+                'd="M22.477.927C10.485.927.76 10.65.76 22.647c0 9.596 6.223 17.736 '
+                "14.853 20.608 1.087.2 1.483-.47 1.483-1.047 "
+                "0-.516-.019-1.881-.03-3.693-6.04 "
+                "1.312-7.315-2.912-7.315-2.912-.988-2.51-2.412-3.178-2.412-3.178-1.972-1.346.149-1.32.149-1.32 "  # noqa
+                "2.18.154 3.327 2.24 3.327 2.24 1.937 3.318 5.084 2.36 6.321 "
+                "1.803.197-1.403.759-2.36 "
+                "1.379-2.903-4.823-.548-9.894-2.412-9.894-10.734 "
+                "0-2.37.847-4.31 2.236-5.828-.224-.55-.969-2.759.214-5.748 0 0 "
+                "1.822-.584 5.972 2.226 "
+                "1.732-.482 3.59-.722 5.437-.732 1.845.01 3.703.25 5.437.732 "
+                "4.147-2.81 5.967-2.226 "
+                "5.967-2.226 1.185 2.99.44 5.198.217 5.748 1.392 1.517 2.232 3.457 "
+                "2.232 5.828 0 "
+                "8.344-5.078 10.18-9.916 10.717.779.67 1.474 1.996 1.474 4.021 0 "
+                "2.904-.027 5.247-.027 "
+                "5.96 0 .58.392 1.256 1.493 1.044C37.981 40.375 44.2 32.24 44.2 "
+                '22.647c0-11.996-9.726-21.72-21.722-21.72" '
+                'fill="currentColor"/></svg>'
+            ),
         },
-    ],
-    "source_repository": "https://github.com/jeertmans/DiffeRT2d/",
-    "source_branch": "main",
-    "source_directory": "docs/source/",
-}
+    },
+)
+
+html_theme_options = asdict(theme_options)
 
 # -- Intersphinx mapping
 
