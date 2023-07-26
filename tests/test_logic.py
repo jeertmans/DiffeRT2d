3496c0e66a789bb7b0dea283acca33d6a4aa3168
Jérome Eertmans <jeertmans@icloud.com>
2023-07-26 14:17:11 +0200

    chore(tests): test activation function

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,9 +1,10 @@
 import chex
 import jax
 import jax.numpy as jnp
+import pytest
 from jax import disable_jit
 
-from differt2d.logic import enable_approx, is_true
+from differt2d.logic import activation, enable_approx, is_true
 
 
 def test_enable_approx():
@@ -93,4 +94,17 @@ def test_enable_approx_with_keyword():
     expected = jnp.array(True)
     got = is_true(True, approx=False)
     chex.assert_trees_all_equal(expected, got)
+    chex.assert_trees_all_equal_shapes_and_dtypes(expected, got)
+
+
+@pytest.mark.parametrize(
+    ("function", "jax_fun"),
+    [("sigmoid", jax.nn.sigmoid), ("hard_sigmoid", jax.nn.hard_sigmoid)],
+)
+@pytest.mark.parametrize(("alpha",), [(1e-3,), (1e-2,), (1e-1,), (1e-0,), (1e1,)])
+def test_activation(function, jax_fun, alpha):
+    x = jnp.linspace(-5, +5, 200)
+    expected = jax_fun(alpha * x)
+    got = activation(x, alpha=alpha, function=function)
+    chex.assert_trees_all_close(expected, got)
     chex.assert_trees_all_equal_shapes_and_dtypes(expected, got)
